677809dec27c6832ab1137b342f2978d33a88d8b
Alexander Coral <coral.alexander@gmail.com>
2018-07-23 22:53:22 +0300

    ver 2.0

--- src/main/java/stream/WorkWithStream.java
+++ src/main/java/stream/WorkWithStream.java
@@ -1,11 +1,16 @@
 package stream;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 public class WorkWithStream {
 
     public static void main(String[] args) {
-
 
         ArrayList<String> list = new ArrayList();
 
@@ -16,8 +21,8 @@ public class WorkWithStream {
         list.stream().filter("Kuku"::equals).count();
 
         list.forEach((String value) -> System.out.println(value));
+        List<Integer> numbers = asList(1, 2, 3, 4);
+        //Set<Integer> numbers = new HashSet(asList(4, 3, 2, 1));
+        List<Integer> sameOrder = numbers.stream().sorted().collect(toList());
     }
-
-
-
 }
